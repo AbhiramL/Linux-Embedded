81cf70521c81ade4759be0ef87ecc5719c4a0d94
AbhiramL <73972193+AbhiramL@users.noreply.github.com>
2025-04-13 17:19:37 -0400

    Update userPgrm.c

--- BeagleboneBlack/kernMods/uartMod/userPgrm.c
+++ BeagleboneBlack/kernMods/uartMod/userPgrm.c
@@ -6,7 +6,7 @@
 #include <sys/ioctl.h>
 
 #define DEVICE_FILE "/dev/AL_UART"
-#define SEND_MESSAGE 0x01  // Same as defined in the kernel module
+#define TRANSMIT_MESSAGE 0x01
 
 int main(int argc, char *argv[])
 {
@@ -14,29 +14,26 @@ int main(int argc, char *argv[])
     char message[256];
 
     if (argc != 2) {
-        fprintf(stderr, "Usage: %s <message>\n", argv[0]);
+        fprintf(stderr, "(UserProgram)Usage: %s <message>\n", argv[0]);
         return EXIT_FAILURE;
     }
 
     snprintf(message, sizeof(message), "%s", argv[1]);
 
-    // Open the device file
     fd = open(DEVICE_FILE, O_RDWR);
     if (fd < 0) {
-        perror("Failed to open the device file");
+        perror("(UserProgram)Failed to open device file");
         return EXIT_FAILURE;
     }
 
-    // Send message to the kernel module
-    if (ioctl(fd, SEND_MESSAGE, message) < 0) {
-        perror("Failed to send message via IOCTL");
+    if (ioctl(fd, TRANSMIT_MESSAGE, message) < 0) {
+        perror("(UserProgram)Failed to send message via IOCTL");
         close(fd);
         return EXIT_FAILURE;
     }
 
-    printf("Message sent: %s\n", message);
+    printf("(UserProgram)Message sent to UART: %s\n", message);
 
     close(fd);
     return EXIT_SUCCESS;
 }
-
